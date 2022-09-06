bd5c3ed0ea9c1e96d94a7fa80132cecaa1bcdde4
Quade Houston <quadehouston@gmail.com>
2022-09-05 22:27:03 -0400

    commit tree a1312f7fcb9cd5cd1361e4d47347de1dde64a30f

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,9 @@
 class Game < ActiveRecord::Base
-  
+    has_many :reviews # must be plural
+    # written method ->
+    # # a review belongs to a game
+    # def game
+    #     #self is the review instance
+    #     Game.find(self.game_id)
+    # end
 end
